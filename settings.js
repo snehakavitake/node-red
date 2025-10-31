d3c295faceed54e084de90ded5194c637aa576de
snehakavitake <146338468+snehakavitake@users.noreply.github.com>
2025-10-31 22:03:56 +0530

    Add settings.js for configuration
    Created settings.js with default configuration.

---
+++ settings.js
@@ -0,0 +1,13 @@
+// settings.js - optional
+module.exports = {
+  uiPort: process.env.PORT || 1880,
+  // To secure the editor you must set adminAuth. See Node-RED docs to generate hashed password.
+  // adminAuth: {
+  //    type: "credentials",
+  //    users: [{
+  //      username: "admin",
+  //      password: "<bcrypt-hash>",
+  //      permissions: "*"
+  //    }]
+  // }
+};
